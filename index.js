8a559cb60b6c12b875438ec215cb00be0ca5a869
lordish <gitonga403@gmail.com>
2023-04-04 19:45:14 +0300

    Add db.json data

---
+++ index.js
@@ -0,0 +1,11 @@
+// let letters = document.querySelectorAll('a')
+// for(let letter of letters ){
+//     letter.addEventListener('click', ()=>{
+//         let myLetter = letter.textContent
+//         let smaLetter = myLetter.toLowerCase()
+//         fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?f=${smaLetter}`)
+//         .then(res=>res.json())
+//         .then(data=>console.log(data))
+//     })
+    
+// }

22faa617d97413d1ed2227a8abd659cd151bd3b3
lordish <gitonga403@gmail.com>
2023-04-05 17:16:47 +0300

    Add a pop-up view to view each item by detail

--- index.js
+++ index.js
@@ -1,41 +1,71 @@
-const groceryItems = document.getElementById('my-display');
+document.addEventListener('DOMContentLoaded', function(){
 
-function showItems () {
-    fetch('http://localhost:3000/groceries')
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        const groceries = data;
-        let myListArr = groceries.map(list=>{
-            let groceryObj = {
-                name : list.name,
-                image : list.img,
-                price : list.price,
-                description : list.description
-            }
-            return groceryObj;
+    const groceryItems = document.getElementById('my-display');
+    //const displaySome = document.getElementsByClassName('card');
+    
+    const popUp = document.getElementById('popup');
+    const popupContent = document.getElementById('popup-content')
+   // const popupImage = document.getElementById('popup-image');
+    //popupImage.innerHTML = '<img src="" alt="Popup Image">';
+    //popupImage.style.maxWidth = '100%';
+    //popupImage.style.maxHeight = '100%';
+    //popupImage.style.overflow = 'auto';
+    const popupName = document.getElementById('popup-name')
+    const popupDescription = document.getElementById('popup-description')
+    const popupPrice = document.getElementById('popup-price')
+    //const closeBtn = document.getElementById('close-btn')
+    
+    function showItems () {
+        fetch('http://localhost:3000/groceries')
+        .then(function(response){
+            return response.json()
         })
-        //console.log(myListArr)
-        myListArr.map(list=>displayItem(list))
+        .then(function(data){
+            const groceries = data;
+            let myListArr = groceries.map(list=>{
+                let groceryObj = {
+                    name : list.name,
+                    image : list.img,
+                    price : list.price,
+                    description : list.description
+                }
+                return groceryObj;
+            })
+            //console.log(myListArr)
+            myListArr.map(list=>displayItem(list))
+    
+        //return myListArr(displayItem())
+        })
+    }
 
-    //return myListArr(displayItem())
-    })
-}
- 
-function displayItem (groceryObj){
-     let myList = document.createElement('li')
-     myList.className = 'card'
-     myList.innerHTML = `
-     <div class="card col-m" style="width: 18rem;">
-         <img src="${groceryObj.image}" class="card-img-top" alt="${groceryObj.name}">
-         <div class="card-body">
-           <p class="card-text">${groceryObj.name}</p>
-           <p>${groceryObj.price}</p>
-         </div>
-       `
-    groceryItems.appendChild(myList);
+    showItems();
+     
+    function displayItem (groceryObj){
+         let myList = document.createElement('li')
+         myList.className = 'card'
+         myList.innerHTML = `
+         <div class="card" style="width: 18rem;">
+             <img src="${groceryObj.image}" class="card-img-top" alt="${groceryObj.name}">
+             <div class="card-body">
+              <p class="card-text"><a href="#">${groceryObj.name}</a></p>
+               <p>${groceryObj.price}</p>
+             </div>
+        </div>
+        `;
+           groceryItems.appendChild(myList);
+           const itemText = myList.querySelector('.card-text a');
+           itemText.addEventListener('click', function(){
+            //popupImage.querySelector('img').setAttribute('src', groceryObj.image);
+            popupName.textContent = groceryObj.name;
+            popupDescription.textContent = groceryObj.description;
+            popupPrice.textContent = groceryObj.price;
+            popUp.style.display = 'block';
+           });
+    } 
 
-}
-
- showItems()
+    const closeBtn = document.getElementById('close-btn');
+    closeBtn.addEventListener('click', function(){
+        popupContent.style.display = 'none';
+    });
+    
+});
